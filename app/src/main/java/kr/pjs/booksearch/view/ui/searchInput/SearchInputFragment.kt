a4376ebacec6fb102ef18362410d3eebcae0e81d
Jaeseong Park <pjs3065@gmail.com>
2021-03-11 01:56:41 +0900

    refactor : viewModelFactory 적용

--- app/src/main/java/kr/pjs/booksearch/view/ui/searchInput/SearchInputFragment.kt
+++ app/src/main/java/kr/pjs/booksearch/view/ui/searchInput/SearchInputFragment.kt
@@ -2,9 +2,11 @@ package kr.pjs.booksearch.view.ui.searchInput
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import kr.pjs.booksearch.R
 import kr.pjs.booksearch.databinding.FragmentSearchInputBinding
+import kr.pjs.booksearch.extensions.getViewModelFactory
 import kr.pjs.booksearch.view.base.DataBindingFragment
 
 /**
@@ -12,8 +14,7 @@ import kr.pjs.booksearch.view.base.DataBindingFragment
  */
 class SearchInputFragment :
     DataBindingFragment<FragmentSearchInputBinding>(R.layout.fragment_search_input) {
-    override val viewModel: ViewModel
-        get() = SearchInputViewModel()
+    override val viewModel by viewModels<SearchInputViewModel> { getViewModelFactory() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/kr/pjs/booksearch/view/ui/searchResult/SearchResultFragment.kt
+++ app/src/main/java/kr/pjs/booksearch/view/ui/searchResult/SearchResultFragment.kt
@@ -2,9 +2,10 @@ package kr.pjs.booksearch.view.ui.searchResult
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.viewModels
 import kr.pjs.booksearch.R
 import kr.pjs.booksearch.databinding.FragmentSearchResultBinding
+import kr.pjs.booksearch.extensions.getViewModelFactory
 import kr.pjs.booksearch.view.base.DataBindingFragment
 
 /**
@@ -12,8 +13,7 @@ import kr.pjs.booksearch.view.base.DataBindingFragment
  */
 class SearchResultFragment :
     DataBindingFragment<FragmentSearchResultBinding>(R.layout.fragment_search_result) {
-    override val viewModel: ViewModel
-        get() = SearchResultViewModel()
+    override val viewModel by viewModels<SearchResultViewModel> { getViewModelFactory() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
